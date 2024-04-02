132579afc62e72a612fd6bf4219fe76ca75d875b
João Henz <joao.henz@poatek.com>
2024-04-02 11:48:03 -0300

    🏷️ Add movie model

--- Kudocad/Kudocad/Kudocad/Quickstart/models.py
+++ Kudocad/Kudocad/Kudocad/Quickstart/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=128)
+    mpaa_rating = models.CharField(max_length=32)
+    budget = models.IntegerField()
+    gross = models.IntegerField()
+    release_date = models.DateField()
+    genre = models.CharField(max_length=64)
+    runtime = models.IntegerField()
+    rating = models.DecimalField(decimal_places=2, max_digits=6)
+    rating_count = models.IntegerField()
+    summary = models.CharField(max_length=1024)
