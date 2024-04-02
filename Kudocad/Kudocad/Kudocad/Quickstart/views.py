ad5cdc8cdb7f3e4ff04e50c3786501ef6620450f
João Henz <joao.henz@poatek.com>
2024-04-02 14:39:20 -0300

    🏷️ Add MovieViewSet

--- Kudocad/Kudocad/Kudocad/Quickstart/views.py
+++ Kudocad/Kudocad/Kudocad/Quickstart/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
+from Kudocad.Quickstart.models import Movie
 
-from Kudocad.Quickstart.serializers import GroupSerializer, UserSerializer
+from Kudocad.Quickstart.serializers import GroupSerializer, MovieSerializer, UserSerializer
 
 
 
@@ -20,4 +21,10 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all().order_by('title')
+    serializer_class = MovieSerializer
     permission_classes = [permissions.IsAuthenticated]
