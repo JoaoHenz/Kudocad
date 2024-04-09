36821e67de530aa6c4e4369d3fe0b4daa8dbf87c
João Henz <joao.henz@poatek.com>
2024-04-09 11:34:53 -0300

    ➕ Add django_filters to installed apps

--- Kudocad/settings.py
+++ Kudocad/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'Kudocad.Quickstart'
+    'Kudocad.Quickstart',
+    'django_filters'
 ]
 
 # Middleware framework
